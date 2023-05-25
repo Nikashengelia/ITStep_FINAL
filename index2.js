03c82da4011ecf0af18236614b4883d1621e951e
Nikashengelia <nikashengelia245@gmail.com>
2023-05-25 16:38:42 +0400

    new-6

--- index2.js
+++ index2.js
@@ -5,3 +5,11 @@ function redirectToPage(page) {
 function redirectToPage2(page) {
   window.location.href = "index.html";
 }
+$(document).ready(function() {
+  $('.register-link').click(function(e) {
+    e.preventDefault();
+    $('.login').hide();
+    $('.register').show();
+  });
+}
+  )
