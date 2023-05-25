33aa7c9ae6df902a92dbbc5e9fc303a4592837d8
Nikashengelia <nikashengelia245@gmail.com>
2023-05-25 16:35:57 +0400

    new-6

--- index.js
+++ index.js
@@ -35,3 +35,61 @@ function goToActivity() {
 function showAllHotels() {
   window.location.href = "hotels.html";
 }
+function showAllTips() {
+  window.location.href = "tips.html";
+}
+//სერჩი
+// Define an array of activities with sample data
+const activities = [
+  { category: 'Adventure', location: 'New York' },
+  { category: 'Sightseeing', location: 'Paris' },
+  // Add more activity objects here
+];
+
+// Search form submission function
+function handleSearch(event) {
+  event.preventDefault(); // Prevent form submission
+
+  const category = document.getElementById('category-select').value;
+  const location = document.getElementById('location-input').value.trim().toLowerCase();
+
+  const filteredActivities = activities.filter(activity => {
+    if (category === 'all' || activity.category === category) {
+      return activity.location.toLowerCase().includes(location);
+    }
+    return false;
+  });
+
+  // Display filtered results
+  showResults(filteredActivities);
+}
+
+// Function to display search results
+function showResults(activities) {
+  const resultsContainer = document.getElementById('results-container');
+  resultsContainer.innerHTML = ''; // Clear previous results
+
+  if (activities.length > 0) {
+    activities.forEach(activity => {
+      const activityElement = document.createElement('div');
+      // Customize the activity element's content and style as needed
+
+      resultsContainer.appendChild(activityElement);
+    });
+  } else {
+    // No result found, show message
+    const messageElement = document.createElement('p');
+    messageElement.textContent = 'Activities not found.';
+    resultsContainer.appendChild(messageElement);
+  }
+}
+
+// Get the search form element
+const searchForm = document.getElementById('search-form');
+
+// Add an event listener to the search form submission
+searchForm.addEventListener('submit', handleSearch);
+
+
+
+
--- index2.js
+++ index2.js
@@ -1,19 +1,7 @@
-function redirectToPage() {
-  
-    var url = "index.html";
-    window.location.href = url;
-  }
 
-  $(document).ready(function() {
-    $('.register-link').click(function(e) {
-      e.preventDefault();
-      $('.login').hide();
-      $('.register').show();
-    });
-
-    $('.login-link').click(function(e) {
-      e.preventDefault();
-      $('.register').hide();
-      $('.login').show();
-    });
-  });
+function redirectToPage(page) {
+  window.location.href = "index.html";
+}
+function redirectToPage2(page) {
+  window.location.href = "index.html";
+}
